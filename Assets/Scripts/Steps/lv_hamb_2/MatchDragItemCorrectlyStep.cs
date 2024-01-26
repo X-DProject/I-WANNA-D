3a019429c857cab9cee6916be1171b35fe9212fe
S.H.W <2536076378@qq.com>
2024-01-26 20:53:21 +0800

    dev: rename

---
+++ Assets/Scripts/Steps/lv_hamb_2/MatchDragItemCorrectlyStep.cs
@@ -0,0 +1,28 @@
+﻿using Game.Behav;
+using UnityEngine;
+
+namespace Game.Step.LvHamb2
+{
+    public sealed class MatchDragItemCorrectlyStep : LevelStep
+    {
+        [SerializeField]
+        private PositionMatcher _matchArea;
+
+        [SerializeField]
+        private DraggableObject _dragItem;
+
+        public void Check()
+        {
+            if (_matchArea == null)
+                throw new MissingComponentException();
+
+            if (_matchArea.HasMatch)
+            {
+                this.IsOk = true;
+
+                _dragItem.transform.position = _matchArea.transform.position;
+                _dragItem.enabled = false;
+            }
+        }
+    }
+}
