cdc7897953ddd55cc18229db93fe4ef2d5e8858f
S.H.W <2536076378@qq.com>
2024-01-27 16:32:20 +0800

    dev: Lv Helper

---
+++ Assets/Scripts/Utilities/LevelHelper.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Game.Util
+{
+    public sealed class LevelHelper : MonoBehaviour
+    {
+        public void Win()
+        {
+            GameInstance.Signal("next_level");
+        }
+    }
+}
