8d5904e13d6b1975d7f982314b5bc09e9be4a7cc
S.H.W <2536076378@qq.com>
2024-01-27 13:47:44 +0800

    dev: lv2 文字变色behaviour

--- Assets/Scripts/Behav/Text/TMProColorChanger.cs
+++ Assets/Scripts/Behav/Text/TMProColorChanger.cs
@@ -26,13 +26,35 @@ namespace Game.Behav.Text
             var alphaTurningInfo = arg.Split(' ');
 
             if (alphaTurningInfo.Length != 2)
-                throw new ArgumentException("[alphaTurningInfo] must have 2 argument for lerp turning.");
+                throw new ArgumentException("[alphaTurningInfo] must have 2 argument for alpha lerp turning: alpha duration");
 
             DOTween.To(
                 getter: () => _text.alpha,
                 setter: a  => _text.alpha = a,
                 endValue: float.Parse(alphaTurningInfo[0]),
                 duration: float.Parse(alphaTurningInfo[1])
+                );
+        }
+
+        public void ChangeColorLerply(string arg)
+        {
+            if (string.IsNullOrEmpty(arg))
+                throw new ArgumentNullException(nameof(arg));
+
+            var colorTurningInfo = arg.Split(' ');
+
+            if (colorTurningInfo.Length != 4)
+                throw new ArgumentException("[alphaTurningInfo] must have 4 argument for color lerp turning: r g b duration");
+
+            DOTween.To(
+                getter: () => _text.color,
+                setter: c => _text.color = c,
+                endValue: new Color(
+                    r: float.Parse(colorTurningInfo[0]) / 255,
+                    g: float.Parse(colorTurningInfo[1]) / 255,
+                    b: float.Parse(colorTurningInfo[2]) / 255
+                    ),
+                duration: float.Parse(colorTurningInfo[3])
                 );
         }
     }
