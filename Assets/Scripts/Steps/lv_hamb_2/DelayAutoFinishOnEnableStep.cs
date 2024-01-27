9497330a830103d50059dbb1010db99b581fc0f8
S.H.W <2536076378@qq.com>
2024-01-27 14:16:22 +0800

    dev: delay step

---
+++ Assets/Scripts/Steps/lv_hamb_2/DelayAutoFinishOnEnableStep.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Game.Step
+{
+    public class DelayAutoFinishOnEnableStep : LevelStep
+    {
+        [Header("Behav")]
+
+        [SerializeField]
+        private float _finishDelayTime;
+
+        private void OnEnable()
+        {
+            StartCoroutine(Run());
+        }
+
+        private IEnumerator Run()
+        {
+            yield return new WaitForSecondsRealtime(_finishDelayTime);
+            this.Check();
+        }
+
+        protected override bool TryPassStep()
+        {
+            return true;
+        }
+    }
+}
